232541f583eb43110974da2d304b7bd22a379b94
Rupesh267 <rupeshdangodra2004@gmail.com>
2022-05-21 11:57:31 +0530

    21/05/22

--- app/src/main/java/com/example/ebooks/MainActivity.kt
+++ app/src/main/java/com/example/ebooks/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.ebooks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ebooks.Screen.Home_Fragment.view.Home_Fragment
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
+
+
+    fun loadFragment()
+    {
+        var fragmentTransient = supportFragmentManager.beginTransaction()
+        fragmentTransient.replace(R.id.frgment_frame_layout,Home_Fragment())
+        fragmentTransient.commit()
+
+    }
+
+
 }
